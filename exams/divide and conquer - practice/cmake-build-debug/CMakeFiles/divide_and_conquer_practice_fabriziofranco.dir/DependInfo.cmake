009ca333e148f2f36f67124ffc9dc30cd213688b
fabriziofranco <fabrizio.franco@utec.edu.pe>
2021-01-20 14:29:04 -0500

    Working

---
+++ exams/divide and conquer - practice/cmake-build-debug/CMakeFiles/divide_and_conquer_practice_fabriziofranco.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fabrizio/Documents/divide-and-conquer-practice-fabriziofranco/answers/problem1/problem1.cpp" "/home/fabrizio/Documents/divide-and-conquer-practice-fabriziofranco/cmake-build-debug/CMakeFiles/divide_and_conquer_practice_fabriziofranco.dir/answers/problem1/problem1.cpp.o"
+  "/home/fabrizio/Documents/divide-and-conquer-practice-fabriziofranco/answers/problem2/problem2.cpp" "/home/fabrizio/Documents/divide-and-conquer-practice-fabriziofranco/cmake-build-debug/CMakeFiles/divide_and_conquer_practice_fabriziofranco.dir/answers/problem2/problem2.cpp.o"
+  "/home/fabrizio/Documents/divide-and-conquer-practice-fabriziofranco/answers/problem3/problem3.cpp" "/home/fabrizio/Documents/divide-and-conquer-practice-fabriziofranco/cmake-build-debug/CMakeFiles/divide_and_conquer_practice_fabriziofranco.dir/answers/problem3/problem3.cpp.o"
+  "/home/fabrizio/Documents/divide-and-conquer-practice-fabriziofranco/answers/problem4/problem4.cpp" "/home/fabrizio/Documents/divide-and-conquer-practice-fabriziofranco/cmake-build-debug/CMakeFiles/divide_and_conquer_practice_fabriziofranco.dir/answers/problem4/problem4.cpp.o"
+  "/home/fabrizio/Documents/divide-and-conquer-practice-fabriziofranco/answers/problem5/problem5.cpp" "/home/fabrizio/Documents/divide-and-conquer-practice-fabriziofranco/cmake-build-debug/CMakeFiles/divide_and_conquer_practice_fabriziofranco.dir/answers/problem5/problem5.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
